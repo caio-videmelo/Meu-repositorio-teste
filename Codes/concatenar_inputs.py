20ce7f394da2a10d87825e30570f65c53d7f23df
Caio Melo <caio.videmelo@gmail.com>
2024-07-23 10:08:35 -0300

    Create concatenar_inputs.py

---
+++ Codes/concatenar_inputs.py
@@ -0,0 +1,7 @@
+# Vamos receber dois dados diferentes do usuário e concatenar
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+info_concatenada = info1 + " " + info2
+
+print("As informações concatenadas são ", info_concatenada)
