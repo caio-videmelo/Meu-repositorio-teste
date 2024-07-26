a569315253c3de5c7dccc84c8a2b5211c310cd13
Caio Melo <caio.videmelo@gmail.com>
2024-07-26 09:12:33 -0300

    Update concatenar_inputs.py
    PEP8 guidelines

--- Codes/concatenar_inputs.py
+++ Codes/concatenar_inputs.py
@@ -1,7 +1,16 @@
-# Vamos receber dois dados diferentes do usuário e concatenar
-info1 = input("Digite a primeira informação: ")
-info2 = input("Digite a segunda informação: ")
+def main():
+    """
+    This function receives two inputs from the user and concatenates them.
+    """
+    # Receive two different pieces of information from the user
+    info1 = input("Digite a primeira informação: ")
+    info2 = input("Digite a segunda informação: ")
 
-info_concatenada = info1 + " " + info2
+    # Concatenate the inputs with a space in between
+    concatenated_info = f"{info1} {info2}"
 
-print("As informações concatenadas são ", info_concatenada)
+    # Print the concatenated information
+    print("As informações concatenadas são:", concatenated_info)
+
+if __name__ == "__main__":
+    main()
